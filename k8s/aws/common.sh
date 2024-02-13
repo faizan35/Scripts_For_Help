319a05a974747ce4107655a6457bf7169e2392bb
Faizan <faizanansari4862@gmail.com>
2024-02-13 20:02:30 +0530

    k8s aws script

---
+++ k8s/aws/common.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update
+sudo swapoff -a
+sudo apt install docker.io -y
+
+sudo systemctl enable --now docker
+
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
+
+
+echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+sudo apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+
+
---
+++ k8s/aws/master.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+sudo kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+sudo kubeadm token create --print-join-command
+
+
