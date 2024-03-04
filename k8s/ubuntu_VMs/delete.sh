14504679c1d60d40f42460e977f62cd2c763c8e9
Faizan <faizanansari4862@gmail.com>
2024-03-04 23:51:00 +0530

    uninstall script changed

--- k8s/ubuntu_VMs/delete.sh
+++ k8s/ubuntu_VMs/delete.sh
@@ -1,37 +1,69 @@
+# #!/bin/bash
+
+# # Reset Kubernetes on both control-plane and node
+# sudo kubeadm reset -f
+
+# # Remove Docker images
+# sudo docker rmi -f $(sudo docker images -aq)
+
+
+# # Remove Docker and Kubernetes packages
+# sudo apt purge -y docker.io kubeadm kubectl kubelet kubernetes-cni
+# sudo apt autoremove -y
+# sudo rm -rf /etc/kubernetes /var/lib/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico
+
+# # Reset ufw (firewall)
+# sudo ufw disable
+# sudo ufw reset
+
+
+# # Remove Weave networking (if applied)
+# kubectl delete -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+# # Remove Kubernetes GPG key and repository
+# sudo rm /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
+# sudo rm /etc/apt/sources.list.d/kubernetes.list
+
+# # Remove kubeconfig
+# rm -rf $HOME/.kube
+
+# # Disable & Uninstall UFW
+# sudo ufw disable
+# sudo apt-get remove ufw
+
+# # Remove ufw dir - for Port
+# sudo rm -rf /etc/ufw
+
+# echo "Kubernetes uninstallation complete."
+
+
+##############################
+
 #!/bin/bash
 
-# Reset Kubernetes on both control-plane and node
-sudo kubeadm reset -f
+# Stop and disable the kubelet service
+sudo systemctl stop kubelet
+sudo systemctl disable kubelet
 
-# Remove Docker images
-sudo docker rmi -f $(sudo docker images -aq)
+# Uninstall the Kubernetes packages
+sudo apt-get remove -y kubelet kubeadm kubectl
 
-
-# Remove Docker and Kubernetes packages
-sudo apt purge -y docker.io kubeadm kubectl kubelet kubernetes-cni
-sudo apt autoremove -y
-sudo rm -rf /etc/kubernetes /var/lib/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico
-
-# Reset ufw (firewall)
-sudo ufw disable
-sudo ufw reset
-
-
-# Remove Weave networking (if applied)
-kubectl delete -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
-
-# Remove Kubernetes GPG key and repository
-sudo rm /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
+# Remove the Kubernetes apt repository configuration
 sudo rm /etc/apt/sources.list.d/kubernetes.list
 
-# Remove kubeconfig
-rm -rf $HOME/.kube
+# Remove the Kubernetes GPG key
+sudo rm /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-# Disable & Uninstall UFW
-sudo ufw disable
-sudo apt-get remove ufw
+# Unmark hold on the packages
+sudo apt-mark unhold kubelet kubeadm kubectl
 
-# Remove ufw dir - for Port
-sudo rm -rf /etc/ufw
+# Clean up any unused packages
+sudo apt-get autoremove -y
 
-echo "Kubernetes uninstallation complete."
+# Optionally, remove Docker if it was installed by the script
+if dpkg -l docker.io | grep -q installed; then
+  sudo apt-get remove -y docker.io
+  sudo systemctl disable --now docker
+fi
+
+echo "Kubernetes uninstalled successfully."
