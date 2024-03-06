b91f2e77a662e98d37ad89e10650e25cc2d365e1
Faizan <faizanansari4862@gmail.com>
2024-03-06 22:04:28 +0530

    k8s v1.28.7-00

--- test-k8s/common.sh
+++ test-k8s/common.sh
@@ -8,20 +8,27 @@ sudo apt install docker.io -y
 sudo systemctl enable --now docker
 
 
+# disable swap
+sudo swapoff -a
+
+
 # k8s 1.27
 
 sudo apt-get update
 # apt-transport-https may be a dummy package; if so, you can skip that package
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
 
-# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt-get install -y kubeadm=1.28.7-00 kubectl=1.28.7-00 kubelet=1.28.7-00
+
+
+
+
+
 
 
 # https://dl.k8s.io/apt/doc/apt-key.gpg
