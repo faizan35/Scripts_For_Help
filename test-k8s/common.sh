9e7446485b6cb4719fc74cc8a808dd26f6e10146
Faizan <faizanansari4862@gmail.com>
2024-03-06 21:50:37 +0530

    k8s 1.27

--- test-k8s/common.sh
+++ test-k8s/common.sh
@@ -8,20 +8,42 @@ sudo apt install docker.io -y
 sudo systemctl enable --now docker
 
 
-# k8s 1.28
+# k8s 1.27
 
 sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+
+
+
+# k8s 1.28
+
+# sudo apt-get update
+
+# sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+
+# sudo apt update
+# sudo apt install -y kubelet kubeadm kubectl
+# sudo apt-mark hold kubelet kubeadm kubectl
+
+
+
+############################################################
 
 
 ### Pod Network
--- test-k8s/master.sh
+++ test-k8s/master.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+sudo kubeadm init
+
+# kubeadm init --apiserver-advertise-address 192.168.0.11 --pod-network-cidr 10.0.0.0/16
+
+# kubeadm init --control-plane-endpoint 192.168.0.11
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-sudo kubeadm init
 
 
 sudo kubeadm token create --print-join-command
