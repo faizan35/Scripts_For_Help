c40fc311b80cefe9f359c697e265d852dda16af5
Faizan <faizanansari4862@gmail.com>
2024-03-08 22:22:41 +0530

    sudo added

--- xtest-k8s/common.sh
+++ xtest-k8s/common.sh
@@ -29,11 +29,11 @@ systemctl daemon-reload
 git clone https://github.com/Mirantis/cri-dockerd.git
 cd cri-dockerd
 mkdir -p /usr/local/bin
-install -o root -g root -m 0755 cri-dockerd /usr/local/bin/cri-dockerd
-install packaging/systemd/* /etc/systemd/system
+sudo install -o root -g root -m 0755 cri-dockerd /usr/local/bin/cri-dockerd
+sudo install packaging/systemd/* /etc/systemd/system
 sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
-systemctl daemon-reload
-systemctl enable --now cri-docker.socket
+sudo systemctl daemon-reload
+sudo systemctl enable --now cri-docker.socket
 #######################################
 
 # sudo apt install docker.io -y
