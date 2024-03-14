f63f769723e99cd96ac86f163767e625ba34b4d5
Faizan <faizanansari4862@gmail.com>
2024-03-15 00:54:58 +0530

    master configs settings removed

--- xtest-k8s/master.sh
+++ xtest-k8s/master.sh
@@ -17,22 +17,23 @@ mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-# Save Configs
-config_path="/etc/kubernetes"
+# # Save Configs
+# config_path="/etc/kubernetes"
 
-sudo mkdir -p $config_path/configs
-sudo cp -i /etc/kubernetes/admin.conf $config_path/configs/config
-sudo chmod 644 $config_path/configs/config
+# sudo mkdir -p $config_path/configs
+# sudo cp -i /etc/kubernetes/admin.conf $config_path/configs/config
+# sudo chmod 644 $config_path/configs/config
 
 # Install Calico Network Plugin
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml -O
 
 kubectl apply -f calico.yaml
 
-# Install Metrics Server
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/metrics-server/main/deploy/kubernetes/metrics-server-deployment.yaml
+# # Install Metrics Server
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/metrics-server/main/deploy/kubernetes/metrics-server-deployment.yaml
 
 
+kubeadm token create --print-join-command
 
 # ================================   PORT   ====================================
 
