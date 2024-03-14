6f7fd4bfea705710afe5738765351793d2fb6249
Faizan <faizanansari4862@gmail.com>
2024-03-14 23:57:51 +0530

    control plane ip fixed

--- xtest-k8s/master.sh
+++ xtest-k8s/master.sh
@@ -4,11 +4,14 @@
 
 set -euxo pipefail
 
+# Replace the control plane IP with the appropriate IP address
+control_plane_ip="192.168.0.11"
+
 sudo kubeadm config images pull
 
 echo "Preflight Check Passed: Downloaded All Required Images"
 
-sudo kubeadm init
+sudo kubeadm init --apiserver-advertise-address=$control_plane_ip --apiserver-cert-extra-sans=$control_plane_ip --pod-network-cidr=172.16.1.0/16 --service-cidr=172.17.1.0/18 --node-name "$(hostname -s)" --ignore-preflight-errors Swap
 
 mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
