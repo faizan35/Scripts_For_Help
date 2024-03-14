e79401c39ce1b23ed43cfcfdda8ad1d7a0fe2066
Faizan <faizanansari4862@gmail.com>
2024-03-15 01:07:18 +0530

    docker added in the script

--- xtest-k8s/common.sh
+++ xtest-k8s/common.sh
@@ -36,17 +36,31 @@ sudo sysctl --system
 sudo apt-get update -y
 sudo apt-get install -y software-properties-common curl apt-transport-https ca-certificates gpg
 
-sudo curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
+## 5555555555555555
 
-sudo apt-get update -y
-sudo apt-get install -y cri-o
+sudo apt install docker.io -y
 
-sudo systemctl daemon-reload
-sudo systemctl enable crio --now
-sudo systemctl start crio.service
+sudo systemctl enable --now docker
 
-echo "CRI runtime installed successfully"
+## 5555555555555555
+
+
+##########################################################################
+
+
+# sudo curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+# echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
+
+# sudo apt-get update -y
+# sudo apt-get install -y cri-o
+
+# sudo systemctl daemon-reload
+# sudo systemctl enable crio --now
+# sudo systemctl start crio.service
+
+# echo "CRI runtime installed successfully"
+
+##########################################################################
 
 # Add Kubernetes APT repository and install required packages
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
@@ -54,6 +68,7 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 
 
 sudo apt-get update -y
+# sudo apt-get install -y kubelet kubectl kubeadm
 sudo apt-get install -y kubelet="1.29.0-*" kubectl="1.29.0-*" kubeadm="1.29.0-*"
 sudo apt-get update -y
 sudo apt-get install -y jq
