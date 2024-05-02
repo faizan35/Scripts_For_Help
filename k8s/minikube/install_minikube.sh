29e0e9142405bb29ce7900441f157eac5f49d8c9
faizan35 <faizanansari4862@gmail.com>
2024-05-02 09:50:18 +0530

    minikube

---
+++ k8s/minikube/install_minikube.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update package index
+sudo apt update
+
+# Install VirtualBox
+sudo apt install -y virtualbox docker.io
+
+# Download Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+
+# Install Minikube
+sudo dpkg -i minikube_latest_amd64.deb
+
+# Install kubectl
+sudo apt install -y curl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo "Minikube and kubectl installation completed."
